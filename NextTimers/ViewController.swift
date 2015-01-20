041e203303e367f554257323ed87e341ec7dab5e
Jonas Dandanell <jonasdandanell@Jonass-MacBook-Pro-2.local>
2015-01-20 18:16:34 +0100

    Initial Commit

---
+++ NextTimers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NextTimers
+//
+//  Created by Jonas Dandanell on 2015-01-20.
+//  Copyright (c) 2015 Dandanellfoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
